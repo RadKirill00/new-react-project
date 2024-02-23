1337cb214fcf4b58268e51070df1ea44d5568c98
RadKirill <radkirill00@mail.ru>
2024-02-24 02:20:06 +0300

    qwe

--- my-app/src/components/UseEffect.jsx
+++ my-app/src/components/UseEffect.jsx
@@ -1,20 +1,9 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React from 'react'
 
 const UseEffect = () => {
-  const [count, setCount] = useState(0);
-    const renderCount = useRef(1)
-
-  useEffect(() => {
-    renderCount.current++
-  });
-
   return (
-    <div className="">
-      <h1>{count}</h1>
-      <h1>{renderCount.current}</h1>
-      <button onClick={() => setCount(count + 1)}> Нажми меня</button>
-    </div>
-  );
-};
+    <div>LOl</div>
+  )
+}
 
-export default UseEffect;
+export default UseEffect
